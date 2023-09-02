c35dd96bd3b3d43ae0c62e10e479a817dc133c2a
alifhasanshah <alifhasanshah@gmail.com>
2023-09-02 12:02:53 +0600

    navbar and category data loaded

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('js file connected');
+const loadCategories = async () => {
+  try {
+    const res = await fetch(
+      "https://openapi.programming-hero.com/api/videos/categories"
+    );
+    const data = await res.json();
+    const categoriesData = data.data;
+    categoriesData.forEach((category) => {
+      const categoryContainer = document.getElementById("categories");
+      const categoryDiv = document.createElement("div");
+      categoryDiv.innerHTML = `
+            <button class="px-4 py-2 rounded-lg bg-gray-400 text-white">${category.category}</button>
+            `;
+
+      categoryContainer.appendChild(categoryDiv);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+loadCategories();
